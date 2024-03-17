d042c59cf6c878690fc3492b6d5f6946bb780432
Joe Nguyen <joe.ngyn7@gmail.com>
2024-03-16 20:05:17 -0600

    added bg case study card

--- portfolio_ui/tailwind.config.mjs
+++ portfolio_ui/tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      keyframes: {
+        fade: {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
+      },
+      animation: {
+        fade: "fade 5s ease-in-out 1 forwards",
+      },
+    },
+  },
+  plugins: [],
+};
