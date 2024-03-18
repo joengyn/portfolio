934931a060e4ea2876e58fada92f490cf82e6e7c
Joe Nguyen <joe.ngyn7@gmail.com>
2024-03-17 20:56:34 -0600

    add ui image to case study card

--- portfolio_ui/tailwind.config.mjs
+++ portfolio_ui/tailwind.config.mjs
@@ -8,9 +8,14 @@ export default {
           "0%": { opacity: "1" },
           "100%": { opacity: "0" },
         },
+        moveUp: {
+          "0%": { transform: "translateY(0)" },
+          "100%": { transform: "translateY(-100%)" },
+        },
       },
       animation: {
         fade: "fade 5s ease-in-out 1 forwards",
+        moveUp: "moveUp 2s ease-in-out 1 forwards",
       },
     },
   },
